1b7e75eedcfceac1e7e918b7fa1f9fd444bb9515
lil-dev16 <olusegunkd2017@gmail.com>
2022-09-01 16:21:14 +0100

    jseffects

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const bar = document.querySelector('.hamburger');
+const bar2 = document.getElementById('ham');
 const menu = document.querySelector('.right');
    bar.addEventListener('click', ()=> {
-    menu.classList.toggle('show');
-    
+      const tar = bar.parentElement;
+    tar.classList.toggle('show');
+    if(tar.classList.contains('show')){
+      console.log('hello');
+      bar.innerHTML= '<path fill="#FFF" fill-rule="evenodd" d="M8 5.379L13.303.075l2.122 2.122L10.12 7.5l5.304 5.303-2.122 2.122L8 9.62l-5.303 5.304-2.122-2.122L5.88 7.5.575 2.197 2.697.075 8 5.38z"/>'
+
+    }else{
+      console.log('hi');
+      bar.innerHTML = '<path fill="#242A45" fill-rule="evenodd" d="M0 0h18v3H0V0zm0 6h18v3H0V6zm0 6h18v3H0v-3z"/>'
+    }
    });
    const items = [
       {
@@ -44,15 +53,21 @@ const menu = document.querySelector('.right');
       })
    })
 
-   const arrow = document.querySelectorAll('.arrow');
+   
    const answers = document.querySelectorAll('.answer');
+   const quest = document.querySelectorAll('.cont')
 
-   arrow.forEach(arr=>{
-      arr.addEventListener('click', (e)=>{
-         const elem = e.target.parentElement.parentElement;
-         elem.classList.toggle('show-text');
-         console.log(elem);
-         const le = e.target;
-         le.classList.toggle('turn');
-      })
+   quest.forEach(question=>{
+      const arrow = question.querySelector('.arrow');
+
+   arrow.addEventListener('click', ()=>{
+      quest.forEach(item=> {
+        
+         if(item !== question){
+            item.classList.remove('show-text');
+            console.log('hi');
+         }
+      });
+      question.classList.toggle('show-text');
+   })
    })
